9de5fe2521c02f212b24e5f86cd71f08356bcfcd
adicuco <cucolas.adi@gmail.com>
2020-10-18 14:42:18 +0200

    Add router & basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Router } from "@reach/router";
+
+import HomePage from "./routes/HomePage";
+import Login from "./routes/Login";
+import Register from "./routes/Register";
 
 const App = () => {
-  return <div className="bg-gray-900">hi</div>;
+  return (
+    <div className="w-full h-screen bg-gray-900 text-white">
+      <Router>
+        <HomePage path="/" />
+        <Login path="login" />
+        <Register path="register" />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/routes/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const HomePage = (props) => {
+  return <div>homepage</div>;
+};
+
+HomePage.propTypes = {};
+
+export default HomePage;
