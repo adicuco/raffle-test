7f44c12cb296dc3c9d26303a5eab076d2ea295a2
adicuco <cucolas.adi@gmail.com>
2020-10-18 15:06:46 +0200

    Update Form to collect data

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const Form = ({ onSubmit, submitText, inputs }) => {
+  const [data, setData] = useState(
+    inputs.reduce((obj, { id }) => ({ ...obj, [id]: "" }), {})
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onSubmit(data);
+  };
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.id]: e.target.value,
+    });
+  };
+
   return (
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <form
+      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      onSubmit={handleSubmit}
+    >
       {inputs.map(({ id, label, placeholder, isRequired, type }) => (
-        <div className="mb-4">
+        <div className="mb-4" key={id}>
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor={id}
@@ -17,6 +37,8 @@ const Form = ({ onSubmit, submitText, inputs }) => {
             id={id}
             type={type}
             placeholder={placeholder}
+            value={data[id]}
+            onChange={handleChange}
           />
           <p className="text-red-500 text-xs italic">{label} is required</p>
         </div>
@@ -24,7 +46,7 @@ const Form = ({ onSubmit, submitText, inputs }) => {
       <div className="flex items-center justify-center">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button"
+          type="submit"
         >
           {submitText}
         </button>
