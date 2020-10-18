3b1814da901b93ab34acae1fc7b9cece67a090d9
adicuco <cucolas.adi@gmail.com>
2020-10-18 15:32:17 +0200

    Add redux

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({});
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
