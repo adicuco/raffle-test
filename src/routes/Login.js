02fb1196d600c1763d7923e57ead88a362a5b01e
adicuco <cucolas.adi@gmail.com>
2020-10-18 14:46:27 +0200

    Add Form

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,8 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Form from "../components/Form";
+
 const Login = (props) => {
-  return <div>login</div>;
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 };
 
 Login.propTypes = {};
