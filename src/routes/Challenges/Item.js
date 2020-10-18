b44333c74b4317b6621aec625b40f6fe2d1e1d5f
adicuco <cucolas.adi@gmail.com>
2020-10-18 17:49:46 +0200

    Update Challenges to display list

--- src/routes/Challenges/Item.js
+++ src/routes/Challenges/Item.js
@@ -1,8 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "@reach/router";
 
 const Item = ({ id }) => {
-  return <div>challenge {id}</div>;
+  return (
+    <div className="container flex flex-col justify-center items-center w-full mx-auto my-auto h-screen">
+      <h1 className="text-5xl text-white font-bold pb-5">
+        Challenge <span className="text-blue-500">#{id}</span>
+      </h1>
+
+      <Link
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center"
+        to="/challenges"
+      >
+        Back
+      </Link>
+    </div>
+  );
 };
 
 Item.propTypes = {
--- src/routes/Challenges/List.js
+++ src/routes/Challenges/List.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "@reach/router";
 
 import { getAll } from "../../store/actions/challenges";
 
@@ -11,11 +12,26 @@ const List = (props) => {
 
   useEffect(() => {
     dispatch(getAll());
-  },[dispatch]);
+  }, [dispatch]);
 
   return (
-    <div>
-      hello {user.username}, there are {list.length} challenges
+    <div className="container flex flex-col justify-center items-center w-full mx-auto my-auto h-screen">
+      <h1 className="text-4xl text-white font-bold pb-5">
+        Hello <span className="text-blue-500">{user.username}</span>, choose a
+        challenge
+      </h1>
+
+      <div className="grid grid-cols-3 gap-6">
+        {list.map(({ id }) => (
+          <Link
+            className="rounded border border-blue-500 overfl-hidden shadow-lg cursor-pointer py-4 px-4"
+            key={id}
+            to={`${id}`}
+          >
+            <span className="text-3xl text-blue-300">#{id}</span>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
