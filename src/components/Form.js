3d6b20924060a439939849bc24792dfaadaf7fef
adicuco <cucolas.adi@gmail.com>
2020-10-18 15:11:27 +0200

    Update Form to validate input

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,9 @@ const Form = ({ onSubmit, submitText, inputs }) => {
   const [data, setData] = useState(
     inputs.reduce((obj, { id }) => ({ ...obj, [id]: "" }), {})
   );
+  const [errors, setErrors] = useState(
+    inputs.reduce((obj, { label }) => ({ ...obj, [label]: false }), {})
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,13 +22,27 @@ const Form = ({ onSubmit, submitText, inputs }) => {
     });
   };
 
+  const handleInvalid = (e) => {
+    setErrors({
+      ...errors,
+      [e.target.id]: true,
+    });
+  };
+
+  const removeInvalid = (e) => {
+    setErrors({
+      ...errors,
+      [e.target.id]: false,
+    });
+  };
+
   return (
     <form
       className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
       onSubmit={handleSubmit}
     >
       {inputs.map(({ id, label, placeholder, isRequired, type }) => (
-        <div className="mb-4" key={id}>
+        <div className="mb-5" key={id}>
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor={id}
@@ -33,14 +50,22 @@ const Form = ({ onSubmit, submitText, inputs }) => {
             Password
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2 leading-tight focus:outline-none focus:shadow-outline"
             id={id}
             type={type}
             placeholder={placeholder}
             value={data[id]}
             onChange={handleChange}
+            required={isRequired}
+            onInvalid={handleInvalid}
+            onInput={removeInvalid}
+            style={{
+              borderColor: errors[id] ? "red" : null,
+            }}
           />
-          <p className="text-red-500 text-xs italic">{label} is required</p>
+          <p className="text-red-500 text-xs italic h-1">
+            {errors[id] && `${label} is required`}
+          </p>
         </div>
       ))}
       <div className="flex items-center justify-center">
