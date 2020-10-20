53893c6f14ce88a2e390047ee7cba5036eff8ddc
adicuco <cucolas.adi@gmail.com>
2020-10-20 13:45:55 +0200

    Add Matrix and colors

---
+++ src/components/Matrix.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import colors from "../constants/colors";
+
+const Matrix = ({ matrix }) => {
+  return (
+    <div className="flex w-full h-full flex-col">
+      {matrix.map((row, i) => (
+        <div key={i} className="flex flex-1">
+          {row.map(({ color, player }, j) => (
+            <div
+              key={j}
+              className="flex-1 rounded-sm m-1"
+              style={{
+                backgroundColor: colors[color],
+                opacity: `${player ? 100 : 65}%`,
+              }}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+Matrix.propTypes = {
+  matrix: PropTypes.arrayOf(
+    PropTypes.arrayOf(
+      PropTypes.shape({
+        color: PropTypes.number.isRequired,
+        player: PropTypes.string,
+      })
+    )
+  ).isRequired,
+};
+
+export default Matrix;
---
+++ src/constants/colors.js
@@ -0,0 +1,8 @@
+export default [
+  "#fa0f1bff",
+  "#ff691fff",
+  "#f8f32bff",
+  "#d61fd3ff",
+  "#2fe6deff",
+  "#3bff0aff",
+];
